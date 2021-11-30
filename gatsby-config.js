da30dff32b6cf64fcd6c439634d40ff20571f728
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-11-30 17:36:23 +0100

    Setup Baserow data source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,23 @@
+require('dotenv').config();
+
 module.exports = {
   plugins: [
     'gatsby-theme-material-ui',
+    {
+      resolve: 'gatsby-source-baserow',
+      options: {
+        apiKey: process.env.BASEROW_TOKEN,
+        tables: [
+          {
+            tableId: '38444',
+            tableName: 'Items',
+          },
+          {
+            tableId: '38445',
+            tableName: 'Tags',
+          },
+        ],
+      },
+    },
   ],
 };
