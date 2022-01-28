e7d1e5929f3b585e4aca52d50beb685e21b74fac
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2022-01-28 14:29:20 +0100

    Adjust font sizes

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -1,7 +1,15 @@
 import { createTheme, responsiveFontSizes } from '@mui/material';
 
 const theme = responsiveFontSizes(createTheme({
-  typography: {},
+  typography: {
+    h1: { fontSize: '3rem' },
+    h2: { fontSize: '2.5rem' },
+    h3: { fontSize: '1.75rem' },
+    h4: { fontSize: '1.25rem' },
+    h5: { fontSize: '1rem' },
+    h6: { fontSize: '0.875rem' },
+
+  },
 }));
 
 export default theme;
